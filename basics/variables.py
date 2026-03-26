8bfc105d8afd7db167faf57234441275a13b1463
swarup <swarup.mandhare9822@gmail.com>
2026-03-26 20:17:20 +0530

    updated

--- basics/variables.py
+++ basics/variables.py
@@ -22,3 +22,13 @@ MCP
 LLMS
 """
 print(msg)
+
+
+numbers = [100, 200, 300, 400]
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+print(numbers[-2], numbers[3])   # negative and positive index accessing
+print(numbers[0:3])              # slicing
+print(numbers[-3:-1])            # negative slicing
+print(numbers [:4])              # internally converts to [0:4]
