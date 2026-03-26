f6a187294f515d74a65c0e9829adacf2aef11267
swarup <swarup.mandhare9822@gmail.com>
2026-03-26 09:50:40 +0530

    changed

---
+++ basics/variables.py
@@ -0,0 +1,24 @@
+# num1 = 200      ...single
+# num2 = 300      ...single
+# num1, num2 = 200, 300  ... single line initialization
+# num1 = num2 = 200      ... same line initialization
+# add = num1 + num2
+# print ("Addition", add)
+# print (type(add))
+
+# multi line string (""" """)
+name = "AshokIT"
+address = "Hyderabad"
+course= 'AI'
+msg = """
+ML - Machine Learning
+DL - Deep Learning
+NLP - Natural Language Processing
+Gen AI - Generative AI
+Agents - Agentic AI
+Fine Tuning
+RAG
+MCP
+LLMS
+"""
+print(msg)
